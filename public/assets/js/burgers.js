97fbcbf3d817bf0c3dee4e7396468bbea82a4c2c
Annisa Purbandari <a.purbandari@gmail.com>
2020-11-13 20:22:15 +1100

    fix devour button

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -2,19 +2,22 @@
 $(function () {
     $(".change-devour").on("click", function (event) {
         var id = $(this).data("id");
-        var newDevour = $(this).data("newdevour");
+        console.log("id", id);
+        var newdevour = $(this).data("newdevour");
+        console.log("newdevour", newdevour);
 
         var newDevourState = {
-            devour: newDevour
-        };
+            devour: newdevour,
 
+        };
+        console.log("newDevourState", newDevourState);
         // Send the PUT request.
         $.ajax("/api/burgers/" + id, {
             type: "PUT",
             data: newDevourState
         }).then(
             function () {
-                console.log("changed devour to", newDevour);
+                console.log("changed devour to", newdevour);
                 // Reload the page to get the updated list
                 location.reload();
             }
@@ -27,6 +30,7 @@ $(function () {
         console.log("user_select_burger")
         var newBurger = {
             burger_name: $("#user_select_burger").val().trim(),
+            //devour: $("[burger_name=devour]:checked").val().trim()
         };
         console.log(newBurger);
 
