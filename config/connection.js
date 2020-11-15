c4e5726ad84e6deaec94db4a828fcad6a9f32573
Annisa Purbandari <a.purbandari@gmail.com>
2020-11-15 13:17:14 +1100

    add heroku connection

--- config/connection.js
+++ config/connection.js
@@ -5,12 +5,22 @@ var mysql = require("mysql");
 if (process.env.JAWSDB_URL) {
     connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
+    /*
+    AWS connection
     var connection = mysql.createConnection({
         host: "localhost",
         port: 3306,
         user: "root",
         password: "",
         database: "burger_app_db"
+    });
+    */
+    var connection = mysql.createConnection({
+        host: "erxv1bzckceve5lh.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+        port: 3306,
+        user: "w9cqwyacxgz9e6b3",
+        password: "re3ioce3h1nl1xdl",
+        database: "doe5c0hpd3jfno66"
     });
 };
 
